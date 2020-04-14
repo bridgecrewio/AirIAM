f418034ad8b1e0c83e19ead1f53c4379ec28d8d1
Nimrod Kor <nimrodkor@gmail.com>
2020-04-14 13:20:14 +0300

    Add policy name to inline policy transformer

--- airiam/terraform/entity_terraformers/IAMInlinePolicyTransformer.py
+++ airiam/terraform/entity_terraformers/IAMInlinePolicyTransformer.py
@@ -13,6 +13,7 @@ class IAMInlinePolicyTransformer(BaseEntityTransformer):
     def _generate_hcl2_code(self, entity_json) -> str:
         policy_document_hcl = IAMPolicyDocumentTransformer(entity_json['PolicyDocument'], f"{self._safe_name}_document", self.principal_name)
         return f"""resource "aws_iam_{self._principal}_policy" "{self.principal_name}_{self._safe_name}" {{
+  name   = "{entity_json['PolicyName']}"
   policy = {policy_document_hcl.identifier()}.json
   {self._principal}   = aws_iam_{self._principal}.{self._safe_user_name}.name
 }}
